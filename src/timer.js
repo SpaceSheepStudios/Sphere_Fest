1f9e957f35c944689912eb9055c5ac5f76521396
SpaceSheep <80417265+SpaceSheepStudios@users.noreply.github.com>
2025-03-05 13:34:56 +0100

    Aktualisieren von timer.js

--- src/timer.js
+++ src/timer.js
@@ -1,21 +1,41 @@
 var countDownDate = new Date("Oct 31, 2025 23:59:59").getTime();
 
 var x = setInterval(function() {
-
   var now = new Date().getTime();
-    
   var distance = countDownDate - now;
-    
+
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
+
   document.getElementById("timer").innerHTML = days + "d " + hours + "h "
   + minutes + "m " + seconds + "s ";
-    
+
   if (distance < 0) {
     clearInterval(x);
     document.getElementById("timer").innerHTML = "EXPIRED";
   }
 }, 1000);
+
+function resetCountdown(newDate) {
+  clearInterval(x);
+  countDownDate = new Date(newDate).getTime();
+  x = setInterval(function() {
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("timer").innerHTML = days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s ";
+
+    if (distance < 0) {
+      clearInterval(x);
+      document.getElementById("timer").innerHTML = "EXPIRED";
+    }
+  }, 1000);
+}
